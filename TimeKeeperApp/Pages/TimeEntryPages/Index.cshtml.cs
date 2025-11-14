7aee59ed3327d267ca41a8cfb201b9b963562b71
David R Collins <davidcollins3@msn.com>
2025-11-14 15:49:00 -0500

    Implemented date-based filtering
    Added the ability to filter time entries based on selected date (start of week); works across all roles

--- TimeKeeperApp/Pages/TimeEntryPages/Index.cshtml.cs
+++ TimeKeeperApp/Pages/TimeEntryPages/Index.cshtml.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using TimeKeeperApp.Authorization;
 using TimeKeeperApp.Data;
 using TimeKeeperApp.Models;
-using TimeKeeperApp.Authorization;
 
 namespace TimeKeeperApp.Pages.TimeEntryPages
 {
@@ -24,11 +25,10 @@ namespace TimeKeeperApp.Pages.TimeEntryPages
         }
 
         public IList<TimeEntry> TimeEntry { get;set; }
+        public List<DateOnly> Weeks { get; set; }
 
-        
         [BindProperty(SupportsGet = true)]
-        public IList<int> Weeks { get; set; }
-
+        public string? SelectedWeek { get; set; }
 
         public async Task OnGetAsync()
         {
@@ -40,29 +40,50 @@ namespace TimeKeeperApp.Pages.TimeEntryPages
 
             var currentUserId = UserManager.GetUserId(User);
 
-            var weeks = timeEntries.Select(t => t.Week).Distinct().ToList();
-            ViewData["Weeks"] = weeks;
+            Weeks = timeEntries.Select(t => t.Week).Distinct().ToList();
 
-            //var selectedWeek = Request.Form["SelectWeek"];
 
             // Only your Time Entries are shown
             // UNLESS you're a supervisor or admin
             if (!isAuthorized)
             {
-                timeEntries = timeEntries.Where(t => t.UserID == currentUserId
-                                                //&& t.Week.ToString() == selectedWeek
-                                                );
+                    timeEntries = timeEntries.Where(t => t.UserID == currentUserId
+                                                    //&& t.Week.ToString() == SelectedWeek
+                                                    );
             }
-
             timeEntries = timeEntries.OrderBy(t => t.TimeIn);
-
             TimeEntry = await timeEntries.ToListAsync();
-
-
         }
 
+        public void OnPostWeeks()
+        {
+            var timeEntries = from t in Context.TimeEntry
+                              select t;
 
+            var isAuthorized = User.IsInRole(Constants.AdminRole) ||
+                               User.IsInRole(Constants.SuperRole);
 
+            var currentUserId = UserManager.GetUserId(User);
+
+            Weeks = timeEntries.Select(t => t.Week).Distinct().ToList();
+
+            // Only your Time Entries are shown
+            // UNLESS you're a supervisor or admin
+            // Parse the selected week using the same ISO format the select emits
+            if (!string.IsNullOrEmpty(SelectedWeek)
+                && DateOnly.TryParse(SelectedWeek, out var parsedWeek))
+            {
+                timeEntries = timeEntries.Where(t => t.Week == parsedWeek);
+            }
+
+            // Only your Time Entries are shown UNLESS you're a supervisor or admin
+            if (!isAuthorized)
+            {
+                timeEntries = timeEntries.Where(t => t.UserID == currentUserId);
+            }
+            timeEntries = timeEntries.OrderBy(t => t.TimeIn);
+            TimeEntry = timeEntries.ToList();
+        }
 
         public async Task<IActionResult> OnPostAsync(int id, bool approvalStatus)
         {
--- TimeKeeperApp/obj/Debug/net8.0/TimeKeeperApp.AssemblyInfo.cs
+++ TimeKeeperApp/obj/Debug/net8.0/TimeKeeperApp.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TimeKeeperApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ac6959e9dab09c528a2ee9c2916b8faee47e1aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08d1d6d2748deeae2863fa8abb0f6c59e0de23b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TimeKeeperApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TimeKeeperApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
