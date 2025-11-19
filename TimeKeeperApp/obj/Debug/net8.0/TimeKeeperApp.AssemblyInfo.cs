486470014a1ad720b23056975cdbc7b6e0b95f9c
David R Collins <davidcollins3@msn.com>
2025-11-19 12:41:16 -0500

    Amended "Delete Time Entry" User ID
    Found same problem as previous commit in "Delete" page; "User ID" used to display User's Primary Key, now displays User's Email Address

--- TimeKeeperApp/obj/Debug/net8.0/TimeKeeperApp.AssemblyInfo.cs
+++ TimeKeeperApp/obj/Debug/net8.0/TimeKeeperApp.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TimeKeeperApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7aee59ed3327d267ca41a8cfb201b9b963562b71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0924dd07543bae5e6bd06341ba295da67eb4e978")]
 [assembly: System.Reflection.AssemblyProductAttribute("TimeKeeperApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TimeKeeperApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
